54c4bd6dae4533417c715d72e1fd4d7155ca8207
Rhys Grabany <r.grabany@gmail.com>
2020-06-09 17:12:46 +0100

    Added the ability for the python program to parse images into text

--- mazedisplay.py
+++ mazedisplay.py
@@ -1,28 +1,51 @@
 #!/usr/bin/python3
 from PIL import Image, ImageOps
 
+"""
+    Maze class
+    Holds the text and image relating to a maze
+"""
 class Maze:
 
+    ###############
+    # Constructor #
+    ###############
     def __init__(self, text=None, image=None):
         self.m_Text = text
         self.m_Image = image
-    
+
+    ###########
+    # Setters #
+    ###########
     def setText(self, text):
         self.m_Text = text
     def setImage(self, image):
         self.m_Image = image
-
+    
+    ###########
+    # Getters #
+    ###########
     def getText(self):
         return self.m_Text
     def getImage(self):
         return self.m_Image
 
+    ###########
+    # Methods #
+    ###########
+
+    #Mostly a debugging tool to show the text from parsing text file
     def printText(self):
         for i in self.m_Text:
             print("".join(i))
+    #Method used to save the image to file
     def saveImage(self):
         self.m_Image.save("outImage.png")
 
+"""
+    This method aims to create an image using a textfile input
+    This can work if there is no path or if there is a path
+"""
 def createImage(maze):
     txt = maze.getText()
 
@@ -39,19 +62,20 @@ def createImage(maze):
             elif txt[i][j] == "*":
                 pixels[i, j] = (49, 201, 1)
 
-
+    # For some reason, done normally the image is flipped and rotated, this makes it rightside up
     rotated = im.transpose(Image.ROTATE_270)
     flip = ImageOps.mirror(rotated)
 
     maze.setImage(flip)
 
-
+"""
+    This method aims to parse text from a textfile and store it into the maze object
+"""
 def parseText(maze):
 
     txt = open("outputs/output.txt")
 
     mz = []
-
     for line in txt.readlines():
         li = []
         for i in line.strip():
@@ -60,14 +84,45 @@ def parseText(maze):
 
     maze.setText(mz)
 
+
+def parseImage(maze):
+
+    im = Image.open("outImage.png")
+
+    rotated = im.transpose(Image.ROTATE_270)
+    flip = ImageOps.mirror(rotated)
+
+    txt = []
+
+    pix = flip.load()
+    width, height = flip.size
+    print(width, height)
+    for x in range(0, width):
+        line = []
+        for y in range(0, height):
+            print(x, y)
+            if pix[x, y] == (0, 0, 0):
+                line.append("#")
+            elif pix[x, y] == (255, 255, 255):
+                line.append(" ")
+            elif pix[x, y] == (49, 201, 1):
+                line.append("*")
+        txt.append(line)
+    
+    maze.setText(txt)
+
+
+
+
 def main():
 
     maze = Maze()
 
-    parseText(maze)
-    createImage(maze)
-    maze.saveImage()
-
+    parseImage(maze)
+    maze.printText()
+    #parseText(maze)
+    #createImage(maze)
+    #maze.saveImage()
 
 
 if __name__ == "__main__":
